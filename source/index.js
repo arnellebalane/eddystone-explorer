0c428aa1fb88002b9dc8b5d00ade4d42082023b4
Arnelle Balane <arnellebalane@gmail.com>
2017-12-15 23:13:42 +0800

    setup vuex store

--- source/index.js
+++ source/index.js
@@ -1,8 +1,12 @@
 import Vue from 'vue';
+import Vuex from 'vuex';
 import App from './components/eddystone-explorer.vue';
 import './stylesheets/index.css';
 
+Vue.use(Vuex);
+
 new Vue({
     el: '#app',
+    store: require('./store').default,
     render: h => h(App)
 });
---
+++ source/store.js
@@ -0,0 +1,28 @@
+import Vuex from 'vuex';
+
+export default new Vuex.Store({
+    state: {
+        beacon: null,
+        uuids: {
+            eddystone: 'a3c87500-8ed3-4bdf-8a39-a01bebede295',
+            capabilities: 'a3c87501-8ed3-4bdf-8a39-a01bebede295',
+            activeSlot: 'a3c87502-8ed3-4bdf-8a39-a01bebede295',
+            advertisingInterval: 'a3c87503-8ed3-4bdf-8a39-a01bebede295',
+            radioTxPower: 'a3c87504-8ed3-4bdf-8a39-a01bebede295',
+            advertisedTxPower: 'a3c87505-8ed3-4bdf-8a39-a01bebede295',
+            lockState: 'a3c87506-8ed3-4bdf-8a39-a01bebede295',
+            unlock: 'a3c87507-8ed3-4bdf-8a39-a01bebede295',
+            publicEcdhKey: 'a3c87508-8ed3-4bdf-8a39-a01bebede295',
+            eidIdentityKey: 'a3c87509-8ed3-4bdf-8a39-a01bebede295',
+            advSlotData: 'a3c8750a-8ed3-4bdf-8a39-a01bebede295',
+            factoryReset: 'a3c8750b-8ed3-4bdf-8a39-a01bebede295',
+            remainConnectable: 'a3c8750c-8ed3-4bdf-8a39-a01bebede295'
+        }
+    },
+
+    mutations: {
+        setBeacon(state, beacon) {
+            state.beacon = beacon;
+        }
+    }
+});
