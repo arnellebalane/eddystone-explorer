a67570cd383f44409cee5bf7c32e47a78fe679e6
Arnelle Balane <arnellebalane@gmail.com>
2017-12-15 23:49:52 +0800

    add simple-spinner component

---
+++ source/components/simple-spinner.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="simple-spinner" v-if="show"></div>
+</template>
+
+<script>
+    export default {
+        props: ['show']
+    };
+</script>
+
+<style scoped>
+    .simple-spinner {
+        display: block;
+        width: 1rem;
+        height: 1rem;
+        border: 1px solid rgba(255, 255, 255, 0.5);
+        border-top-color: rgba(255, 255, 255, 0.1);
+        border-radius: 50%;
+        animation: rotate 500ms linear infinite;
+        will-change: transform;
+    }
+
+    @keyframes rotate {
+        from {
+            transform: rotate(0deg);
+        }
+
+        to {
+            transform: rotate(360deg);
+        }
+    }
+</style>
