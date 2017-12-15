aff5088f853d1daaf83cdbe192647a49a1bbe2a0
Arnelle Balane <arnellebalane@gmail.com>
2017-12-15 22:21:35 +0800

    initial work on eddystone-explorer component

--- source/components/eddystone-explorer.vue
+++ source/components/eddystone-explorer.vue
@@ -1,5 +1,11 @@
 <template>
-    <h1>{{ message }}</h1>
+    <div class="eddystone-explorer">
+        <div class="wrapper">
+            <header>
+                <h2>Explore and configure your beacons that support Eddystone, an open beacon format from Google.</h2>
+            </header>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -11,3 +17,38 @@
         }
     };
 </script>
+
+<style scoped>
+    .eddystone-explorer {
+        display: block;
+    }
+
+    .wrapper {
+        width: 95vw;
+        max-width: 532px;
+        margin: 0 auto;
+    }
+
+    header {
+        padding: 2.4rem 1.6rem;
+    }
+
+    h2 {
+        margin: 0;
+        margin-bottom: 2.4rem;
+        font-size: 1.8rem;
+        font-weight: 400;
+    }
+
+    main {
+        padding-bottom: 2.4rem;
+    }
+
+    main > *:not(:first-child) {
+        margin-top: 2.4rem;
+    }
+
+    main > *:not(:last-child) {
+        margin-bottom: 2.4rem;
+    }
+</style>
