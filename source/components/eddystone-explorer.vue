e58564ea5e4af1e37279ddacea61866fb724af92
Arnelle Balane <arnellebalane@gmail.com>
2017-12-15 22:09:17 +0800

    add app entry point script and initial component

---
+++ source/components/eddystone-explorer.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>{{ message }}</h1>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                message: 'Hello World'
+            }
+        }
+    };
+</script>
---
+++ source/index.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+import App from './components/eddystone-explorer.vue';
+
+new Vue({
+    el: '#app',
+    render: h => h(App)
+});
