4a1509d05ec94c683631cdcc9298e022f6a72683
Arnelle Balane <arnellebalane@gmail.com>
2017-12-16 00:08:13 +0800

    define characteristic-value component

---
+++ source/components/characteristic-value.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="characteristic-value">
+        <p class="label">{{ label }}</p>
+
+        <img v-if="value === true" v-bind:src="trueIcon" alt="true">
+        <img v-else-if="value === false" v-bind:src="falseIcon" alt="false">
+        <p v-else-if="value !== undefined" class="value">{{ value }}</p>
+    </div>
+</template>
+
+<script>
+    import check from '../images/check.png';
+    import close from '../images/close.png';
+
+    export default {
+        props: ['label', 'value'],
+        data() {
+            return {
+                trueIcon: check,
+                falseIcon: close
+            };
+        }
+    };
+</script>
+
+<style scoped>
+    .characteristic-value {
+        display: flex;
+        align-items: center;
+        padding: 0 4.2rem;
+    }
+
+    .characteristic-value:nth-of-type(odd) {
+        background-color: rgba(196, 196, 196, 0.05);
+    }
+
+    .characteristic-value:nth-of-type(even) {
+        background-color: rgba(196, 196, 196, 0.1);
+    }
+
+    p {
+        margin: 0;
+        font-size: 1.4rem;
+        line-height: 3.2rem;
+    }
+
+    .label {
+        margin-right: auto;
+    }
+
+    .value {
+        font-family: var(--monospace-font);
+        font-weight: 600;
+    }
+</style>
