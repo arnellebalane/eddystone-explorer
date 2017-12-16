e81822f79905e83e886fec235b81d7c5d5eaed9b
Arnelle Balane <arnellebalane@gmail.com>
2017-12-16 09:00:15 +0800

    define value-with-unit component

---
+++ source/components/value-with-unit.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="value-with-unit">
+        <slot></slot>
+        <span class="unit">{{ unit }}</span>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['unit']
+    };
+</script>
+
+<style scoped>
+    .value-with-unit {
+        display: flex;
+        align-items: center;
+        font-family: var(--monospace-font);
+        font-size: 1.4rem;
+        font-weight: 600;
+    }
+
+    .value-with-unit > * {
+        margin: 0;
+    }
+
+    .unit {
+        display: inline-block;
+        margin-left: 0.8rem;
+        font-weight: 400;
+        opacity: 0.5;
+    }
+</style>
