3c84c172ab584585bb5e8480ebc7130ebb2f6cf4
Arnelle Balane <arnellebalane@gmail.com>
2017-12-16 09:47:04 +0800

    define items-availability component

---
+++ source/components/items-availability.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="items-availability">
+        <slot></slot>
+    </div>
+</template>
+
+<style scoped>
+    .items-availability {
+        display: flex;
+        font-family: var(--monospace-font);
+    }
+
+    .items-availability > * {
+        margin: 0;
+        font-size: 1.4rem;
+    }
+
+    .items-availability > *:not(:first-child) {
+        margin-left: 0.4rem;
+    }
+
+    .items-availability > *:not(:last-child) {
+        margin-right: 0.4rem;
+    }
+
+    .items-availability > .available {
+        font-weight: 600;
+        color: var(--success-color);
+    }
+
+    .items-availability > *:not(.available) {
+        color: var(--danger-color);
+        opacity: 0.5;
+    }
+</style>
